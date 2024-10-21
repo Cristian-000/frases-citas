8dc47a12377d8763d06c672985c09d9aa9206ba2
Cristian Fernández <129860008+Cristian-000@users.noreply.github.com>
2024-10-21 19:22:48 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,32 @@
 // URL del JSON de frases
 const jsonUrl = 'frases.json';
+// Cargar frases de la categoría seleccionada en categoria.html
+if (window.location.pathname.includes('categoria.html')) {
+    const params = new URLSearchParams(window.location.search);
+    const categoriaSeleccionada = params.get('categoria');
+    
+    const tituloCategoria = document.getElementById('titulo-categoria');
+    tituloCategoria.textContent = `Frases de ${categoriaSeleccionada}`;
 
+    fetch(jsonUrl)
+        .then(response => response.json())
+        .then(frases => {
+            const listaFrases = document.getElementById('lista-frases');
+            const frasesCategoria = frases[categoriaSeleccionada];
+
+            if (frasesCategoria && frasesCategoria.length > 0) {
+                frasesCategoria.forEach(fraseObj => {
+                    const li = document.createElement('li');
+                    li.classList.add('list-group-item');
+                    li.innerHTML = `${fraseObj.frase} - <strong>${fraseObj.autor}</strong>`;
+                    listaFrases.appendChild(li);
+                });
+            } else {
+                listaFrases.textContent = "No hay frases para esta categoría.";
+            }
+        })
+        .catch(error => console.error('Error cargando el JSON:', error));
+}
 // Función para obtener una categoría al azar y mostrar una frase del día
 function mostrarFraseDelDia(frases) {
     const categorias = Object.keys(frases);
