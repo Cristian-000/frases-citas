ee6885f1c8f5c92aa7bae7e9ee0d7ec20c345067
Cristian Fernández <129860008+Cristian-000@users.noreply.github.com>
2024-11-12 12:47:46 -0300

    Create autorScript.js

---
+++ autorScript.js
@@ -0,0 +1,40 @@
+function cargarAutor() {
+    return new Promise((resolve, reject) => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const autorSeleccionado = urlParams.get("autor");
+
+        if (!autorSeleccionado) return resolve();
+
+        document.getElementById("titulo-autor").innerText = `Frases de ${autorSeleccionado}`;
+        fetch('frases.json')
+            .then(response => response.json())
+            .then(data => {
+                const listaFrases = document.getElementById("lista-frases");
+                listaFrases.innerHTML = '';
+
+                data.frases.forEach(fraseObj => {
+                    if (fraseObj.autor_url === autorSeleccionado) {
+                        const li = document.createElement("li");
+                        li.className = "list-group-item";
+                        li.innerHTML = `
+                            <p>${fraseObj.frase}</p>
+                            <button onclick="setFraseParaCompartir('${fraseObj.frase}', '${fraseObj.autor_url}')">Usar en Canvas</button>
+                            <div>
+                                <small><a href="autor.html?autor=${fraseObj.autor_url}">${fraseObj.autor_url}</a></small>
+                                ${fraseObj.categorias.map(categoria => `<a href="categoria.html?categoria=${encodeURIComponent(categoria)}" class="badge badge-secondary ml-2">${categoria}</a>`).join(' ')}
+                            </div>
+                        `;
+                        listaFrases.appendChild(li);
+                    }
+                });
+                resolve();
+            })
+            .catch(error => {
+                console.error("Error al cargar frases del autor:", error);
+                reject(error);
+            });
+    });
+}
+
+// Llama a cargarAutor al cargar la página
+document.addEventListener("DOMContentLoaded", cargarAutor);
